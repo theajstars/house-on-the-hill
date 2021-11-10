3136856fd2521f7dbedb64d4252285908d2efe45
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2021-11-10 22:49:06 +0100

    old react-router installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Container } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { ToastProvider } from "react-toast-notifications";
 
 import "./Assets/CSS/All.css";
-import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 function App() {
   useEffect(() => {
     document.title = "Home | House on the hill";
   }, []);
   return (
     <>
+      <Navbar />
       <ToastProvider>
         <div className="home-overlay"></div>
         <div className="home-top"></div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "./App";
 import Navbar from "./Components/Navbar";
 
 ReactDOM.render(
   <Router>
     <App />
-    <Routes>
       <Route exact path="/kids" component={Navbar} />
-    </Routes>
   </Router>,
 
   document.getElementById("root")
